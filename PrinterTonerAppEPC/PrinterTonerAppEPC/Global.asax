e45ede18311765f2a540442c49a371b4a88842e8
nik <npejic@gmail.com>
2017-04-05 22:15:25 +0200

    Started ASP:NET MVC with authorization

---
+++ PrinterTonerAppEPC/PrinterTonerAppEPC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrinterTonerAppEPC.MvcApplication" Language="C#" %>
---
+++ PrinterTonerAppEPC/PrinterTonerAppEPC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrinterTonerAppEPC.Startup))]
+namespace PrinterTonerAppEPC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
